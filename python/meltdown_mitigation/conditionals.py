4561407788b1c2c827deb7b1718bf5eecddcd0e9
yorlysoro <yorlysoro@gmail.com>
2023-09-25 21:17:31 -0400

    improve conditional with pylint

--- python/meltdown_mitigation/conditionals.py
+++ python/meltdown_mitigation/conditionals.py
@@ -1,21 +1,17 @@
 def is_criticality_balanced(temperature, neutrons_emitted):
     temperature_per_neutrons = temperature * neutrons_emitted
-    if temperature < 800 and neutrons_emitted > 500 and temperature_per_neutrons < 500000:
-        return True
-    else:
-        return False
+    return bool(temperature < 800 and neutrons_emitted > 500 and temperature_per_neutrons < 500000)
     
 def reactor_efficiency(voltage, current, theoretical_max_power):
     generated_power = voltage * current
     percent = (generated_power/theoretical_max_power)*100
     if percent >= 80:
         return 'green'
-    elif percent < 80 and percent >= 60:
+    if percent < 80 and percent >= 60:
         return 'orange'
-    elif percent < 60 and percent >= 30:
+    if percent < 60 and percent >= 30:
         return 'red'
-    else:
-        return 'black'
+    return 'black'
 
 def fail_safe(temperature, neutrons_produced_per_second, threshold):
     critical = temperature * neutrons_produced_per_second
@@ -23,6 +19,6 @@ def fail_safe(temperature, neutrons_produced_per_second, threshold):
     high_threshold = threshold * 1.1
     if critical < low_threshold:
         return 'LOW'
-    elif low_threshold <= critical <= high_threshold:
+    if low_threshold <= critical <= high_threshold:
         return 'NORMAL'
     return 'DANGER'
